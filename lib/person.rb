ef25eda48128f53c1bc0cde3aa67e7b901820463
Soren <sorenisme@gmail.com>
2022-11-03 12:02:30 -0400

    commit tree 100d708cb09c9da574ddb22a8e38d92726ac558e

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,47 @@
 # your code goes here
+require 'pry'
+class Person
+    attr_accessor :bank_account
+    attr_reader :name, :hygiene, :happiness
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(num)
+        @happiness = 
+        if num > 10
+            10
+        elsif num < 0
+            0
+        else
+            num
+        end
+    end
+
+    def hygiene=(num)
+        @hygiene = 
+        if num > 10
+            10
+        elsif num < 0
+            0
+        else
+            num
+        end
+    end
+
+    def happy?
+        self.happiness > 7
+    end
+    
+    def clean?
+        self.hygiene > 7
+    end
+
+end
+
+
+
+# binding.pry
